e1d897b39341e97c8f34b62dcdd6fecdc7130f0e
maxenzo10 <98223971+maxenzo10@users.noreply.github.com>
2022-02-09 15:22:44 +0100

    qsd
    qsd

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ btn.addEventListener('click', () => {
 
 	})
 })
+
+/*window.open("https://www.dailymotion.com/playlist/x78viq", "Partenaires Dailymotion", "height=XXX, width=XXX, resizable=yes, menubar=yes, toolbar=no, location=no, status=no, scrollbars=no");
+*/
