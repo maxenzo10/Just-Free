2bcdc6783aee7717b346c42ee4b9affe85ce10b9
maxenzo10 <98223971+maxenzo10@users.noreply.github.com>
2022-02-21 17:23:12 +0100

    a
    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('AVERTISSEMENT')
 	
-alert('ATTENTION : La publicité peut contenir du contenu sensible ou sexuel. Par manque de moyen, nous sommes obligé de travailler avec ce genre de plateforme.')
+alert('ATTENTION : Notre site peut contenir des publicités avec du contenu sensible ou sexuel. Par manque de moyen, nous sommes obligé de travailler avec ce genre de plateforme.')
 
 
 const btn = document.querySelector('.btn');
