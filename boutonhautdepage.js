8fddc3e1e2d9f5f93970febbe7276db3c265f278
maxenzo10 <98223971+maxenzo10@users.noreply.github.com>
2022-07-23 21:45:19 +0100

    test php
    php

---
+++ boutonhautdepage.js
@@ -0,0 +1,11 @@
+const boutontoppage = document.querySelector('.boutontoppage');
+
+boutontoppage.addEventListener('click', () => {
+
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "smooth"
+
+	})
+})
---
+++ boutonhautdepage.php
@@ -0,0 +1,15 @@
+<head>
+<link rel="stylesheet" href="boutonhautdepage.css">
+<script type="text/javascript" src="boutonhautdepage.js" defer></script>
+</head>
+
+
+<!-- Petit bouton pour remonter au top de la page -->
+
+	<div class="boutontoppage">
+
+		<img width="50" height="50" src="https://zupimages.net/up/22/29/kar5.jpg" class="flechetoppage" alt="flèche top page">
+		
+	</div>
+
+<!-- Fin du petit bouton pour aller au top de la page  -->
