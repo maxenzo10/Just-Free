197467c139bcec640d22c0fabf4157bbea71e055
maxenzo10 <98223971+maxenzo10@users.noreply.github.com>
2022-07-23 21:51:07 +0100

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-
-/*window.open("https://www.dailymotion.com/playlist/x78viq", "Partenaires Dailymotion", "height=XXX, width=XXX, resizable=yes, menubar=yes, toolbar=no, location=no, status=no, scrollbars=no");
-*/
-
